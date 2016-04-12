b8569ad70e474f3eb35b8da309b229c1d8eceb73
nameck <470068182@qq.com>
2016-04-12 18:15:19 +0800

    666

--- EasyGo1.0/src/com/easygo/model/beans/chat/NewsPhoto.java
+++
@@ -1,8 +0,0 @@
-package com.easygo.model.beans.chat;
-
-public class NewsPhoto {
-	// 说说图片
-	private int news_photo_id;// 图片id
-	private int news_id;// 所属的说说id
-	private String news_path;// 图片路径
-}
--- EasyGo1.0/src/com/easygo/model/beans/house/HousePhoto.java
+++
@@ -1,8 +0,0 @@
-package com.easygo.model.beans.house;
-
-public class HousePhoto {
-	// 房源图片表（一对多）
-	private int house_photo_id;
-	private int house_id;
-	private String house_photo_path; // 图片地址
-}
