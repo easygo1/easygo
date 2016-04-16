aaf89de221a83fe7eeb2bbfdd3ee24156de69b7e
wsh <wsh@163.com>
2016-04-16 16:31:54 +0800

    111

--- EasyGo/src/com/easygo/model/beans/order/Orders.java
+++ EasyGo/src/com/easygo/model/beans/order/Orders.java
@@ -1,37 +1,53 @@
 package com.easygo.model.beans.order;
 
 import java.sql.Date;
+import java.sql.Timestamp;
 
 public class Orders {
 	private int order_id; // 订单编号
 	private int house_id; // 房屋编号 外键
 	private int user_id; // 房客编号 外键
 	private int checknum; // 入住人数
-	private String checkname; // 入住人姓名
-	private Date checktime; // 入住时间（计算出天数）
-	private Date leavetime; // 离开时间
+	private String checktime; // 入住时间（计算出天数）
+	private String leavetime; // 离开时间
 	private double total; // 订单总额（总价=天数*单价）
 	private String tel; // 联系方式
 	private String order_state; // 订单状态 #待确认，待付款，待入住，已完成，取消订单
+	private String order_time;//下单时间
 
 	public Orders() {
 		super();
 	}
 
-	public Orders(int order_id, int house_id, int user_id, int checknum,
-			String checkname, Date checktime, Date leavetime, double total,
-			String tel, String order_state) {
+	public Orders(int house_id, int user_id, int checknum, String checktime,
+			String leavetime, double total, String tel, String order_state,
+			String order_time) {
 		super();
-		this.order_id = order_id;
 		this.house_id = house_id;
 		this.user_id = user_id;
 		this.checknum = checknum;
-		this.checkname = checkname;
 		this.checktime = checktime;
 		this.leavetime = leavetime;
 		this.total = total;
 		this.tel = tel;
 		this.order_state = order_state;
+		this.order_time = order_time;
+	}
+
+	public Orders(int order_id, int house_id, int user_id, int checknum,
+			 String checktime, String leavetime, double total,
+			String tel, String order_state,String order_time) {
+		super();
+		this.order_id = order_id;
+		this.house_id = house_id;
+		this.user_id = user_id;
+		this.checknum = checknum;
+		this.checktime = checktime;
+		this.leavetime = leavetime;
+		this.total = total;
+		this.tel = tel;
+		this.order_state = order_state;
+		this.order_time=order_time;
 	}
 
 	public int getOrder_id() {
@@ -66,27 +82,19 @@ public class Orders {
 		this.checknum = checknum;
 	}
 
-	public String getCheckname() {
-		return checkname;
-	}
-
-	public void setCheckname(String checkname) {
-		this.checkname = checkname;
-	}
-
-	public Date getChecktime() {
+	public String getChecktime() {
 		return checktime;
 	}
 
-	public void setChecktime(Date checktime) {
+	public void setChecktime(String checktime) {
 		this.checktime = checktime;
 	}
 
-	public Date getLeavetime() {
+	public String getLeavetime() {
 		return leavetime;
 	}
 
-	public void setLeavetime(Date leavetime) {
+	public void setLeavetime(String leavetime) {
 		this.leavetime = leavetime;
 	}
 
@@ -112,6 +120,23 @@ public class Orders {
 
 	public void setOrder_state(String order_state) {
 		this.order_state = order_state;
+	}
+
+	public String getOrder_time() {
+		return order_time;
+	}
+
+	public void setOrder_time(String order_time) {
+		this.order_time = order_time;
+	}
+
+	@Override
+	public String toString() {
+		return "Orders [order_id=" + order_id + ", house_id=" + house_id
+				+ ", user_id=" + user_id + ", checknum=" + checknum
+				+ ", checktime=" + checktime + ", leavetime=" + leavetime
+				+ ", total=" + total + ", tel=" + tel + ", order_state="
+				+ order_state + ", order_time=" + order_time + "]";
 	}
 	
 }
--- EasyGo/src/com/easygo/model/beans/order/UserOrderLinkman.java
+++ EasyGo/src/com/easygo/model/beans/order/UserOrderLinkman.java
@@ -18,37 +18,4 @@ public class UserOrderLinkman {
 		this.name = name;
 		this.idcard = idcard;
 	}
-
-	public int getUser_linkman_id() {
-		return user_linkman_id;
-	}
-
-	public void setUser_linkman_id(int user_linkman_id) {
-		this.user_linkman_id = user_linkman_id;
-	}
-
-	public int getOrder_id() {
-		return order_id;
-	}
-
-	public void setOrder_id(int order_id) {
-		this.order_id = order_id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getIdcard() {
-		return idcard;
-	}
-
-	public void setIdcard(String idcard) {
-		this.idcard = idcard;
-	}
-	
 }
