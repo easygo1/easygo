8f1bed1c1169d6b9e9ab5b84d768e5e1f7dc327c
wsh <wsh@163.com>
2016-05-14 16:35:35 +0800

    +功能

--- EasyGoApp/app/src/main/java/com/easygo/fragment/SearchFragment.java
+++ EasyGoApp/app/src/main/java/com/easygo/fragment/SearchFragment.java
@@ -17,6 +17,8 @@ public class SearchFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+
         return inflater.inflate(R.layout.bottom_search,null);
     }
 }
