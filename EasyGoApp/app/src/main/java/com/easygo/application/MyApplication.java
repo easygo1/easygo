8772cca6732ab00c87f6a7095597c3bc8b19403a
phhuang <329076569@qq.com>
2016-05-26 19:15:00 +0800

    0526

--- EasyGoApp/app/src/main/java/com/easygo/application/MyApplication.java
+++ EasyGoApp/app/src/main/java/com/easygo/application/MyApplication.java
@@ -15,9 +15,9 @@ public class MyApplication extends Application{
     public void onCreate() {
         super.onCreate();
         NoHttp.init(this);
-        x.Ext.init(this);
-        // 是否输出debug日志, 开启debug会影响性能.
-        x.Ext.setDebug(BuildConfig.DEBUG);
+//        x.Ext.init(this);
+//        // 是否输出debug日志, 开启debug会影响性能.
+//        x.Ext.setDebug(BuildConfig.DEBUG);
     }
 
     public String getUrl() {
