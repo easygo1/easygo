4c6224067fa7950da57c5bfb98f7de21f7e3a7fd
张丰鸽 <123@qq>
2016-05-26 18:38:22 +0800

    筛选菜单修改

--- EasyGoApp/app/src/main/java/com/easygo/activity/MyWalletActivity.java
+++ EasyGoApp/app/src/main/java/com/easygo/activity/MyWalletActivity.java
@@ -6,20 +6,18 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class HouseCollectionActivity extends AppCompatActivity {
+public class MyWalletActivity extends AppCompatActivity {
     TextView mTextView;
     ImageView mBackImageView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_house_collection);
+        setContentView(R.layout.activity_my_wallet);
         initViews();
     }
-
     private void initViews() {
         mTextView= (TextView) findViewById(R.id.title_text);
-        mTextView.setText("我的收藏");
+        mTextView.setText("我的钱包");
         mBackImageView= (ImageView) findViewById(R.id.back);
         mBackImageView.setOnClickListener(new View.OnClickListener() {
             @Override
