834c0bc1cc70a663dd4a62718ee6e5052ceb906b
ck <470068182@qq.com>
2016-05-30 13:19:20 +0800

    全删

--- EasyGo/src/com/easygo/utils/DataUtil.java
+++
@@ -1,6 +0,0 @@
-package com.easygo.utils;
-
-public class DataUtil {
-	// 每页显示的数据量
-	public static final int PAGE_SIZE = 2;
-}
--- EasyGoApp/app/src/main/java/com/easygo/beans/User.java
+++
@@ -1,8 +0,0 @@
-package com.easygo.beans;
-
-/**
- * Created by PengHong on 2016/5/18.
- */
-public class User {
-
-}
